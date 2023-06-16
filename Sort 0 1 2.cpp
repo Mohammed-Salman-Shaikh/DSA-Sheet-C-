3bb7e9b2cdf429d80d27da89d48f2466221edc53
Mohammed Salman Shaikh <69892372+Mohammed-Salman-Shaikh@users.noreply.github.com>
2023-06-16 11:20:01 +0530

    Update Sort 0 1 2.cpp

--- Sort 0 1 2.cpp
+++ Sort 0 1 2.cpp
@@ -2,7 +2,7 @@
 
 void sort012(int *arr, int n)
 {
-   // sort(arr, arr+n); //This can solved the sort function
+   // sort(arr, arr+n); //This can solved the sort() function
    int low=0, mid=0, high=n-1;
    while(mid<=high){
       if(arr[mid]==0){
