633015dc70b39753a05080947c50084ba39d8000
Mohammed Salman Shaikh <69892372+Mohammed-Salman-Shaikh@users.noreply.github.com>
2023-06-16 11:19:28 +0530

    Update and rename Sort 0 1 2 to Sort 0 1 2.cpp

--- Sort 0 1 2.cpp
+++ Sort 0 1 2.cpp
@@ -2,8 +2,7 @@
 
 void sort012(int *arr, int n)
 {
-   // sort(arr, arr+n);
-
+   // sort(arr, arr+n); //This can solved the sort function
    int low=0, mid=0, high=n-1;
    while(mid<=high){
       if(arr[mid]==0){
